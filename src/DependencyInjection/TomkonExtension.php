726ef96bb5fa5788f77fbaf6ef2e8d049dd7465a
danielheim <heim@bytesystems.de>
2019-09-05 13:57:32 +0200

    Change folder structure.

--- src/DependencyInjection/TomkonExtension.php
+++ src/DependencyInjection/TomkonExtension.php
@@ -24,7 +24,7 @@ class TomkonExtension extends Extension
     {
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__.'/../Resources/config')
+            new FileLocator(__DIR__ . '/../Resources/config')
         );
 
         $loader->load('services.yml');
