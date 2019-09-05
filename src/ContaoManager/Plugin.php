f89fea89cdfae39313fd793d90ddc5568e5445ec
danielheim <heim@bytesystems.de>
2019-09-05 09:40:16 +0200

    DCA

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,7 +14,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\SkeletonBundle\TomkonBundle;
+use bytesystems\TomkonBundle\TomkonBundle;
 
 class Plugin implements BundlePluginInterface
 {
---
+++ src/Resources/languages/de/tl_member.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS['TL_LANG']['tl_member']['hk_number'] = array
+(
+    'Liegenschaftsnummer',
+    'Bitte geben Sie hier die eindeutige Liegenschaftsnummer ein. Dateien werden über diese Nummer abgeglichen.'
+);
